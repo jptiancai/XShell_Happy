00449a808dd7af6eca055b01f86d2a6c78fd47b5
zhaoyang <mail_jzy@163.com>
2015-06-05 15:02:51 +0800

    add login key and tunnel

---
+++ src/com/sail2world/cons/Constants.java
@@ -0,0 +1,41 @@
+﻿package com.sail2world.cons;
+
+public class Constants {
+	public static final String root = "ScriptPath=C:\\Users\\Administrator\\Documents\\NetSarang\\Xshell\\Scripts\\ScriptSample\\Session\\xshell_session_login.vbs";
+	public static final String font_14 = "FontSize=14";
+	public static final String script_on = "UseInitScript=1";
+	public static final String userName_on = "UserName=jiangzy";
+	public static final String userKey_on = "UserKey=jiangzy_id_rsa";
+	public static final String mysql_sd_7_on = "FwdReqCount=2";
+	public static final String default_path = "C:\\Users\\Administrator\\Documents\\NetSarang\\Xshell\\Sessions\\龙珠Q传\\";
+	public static final String dny_temp_path = default_path+"东南亚服\\东南亚英文\\游戏服\\（东南亚英文04服-05服）54.254.108.188.xsh";
+	public static final String dny_path = default_path + "东南亚服\\东南亚英文";
+	public static final String script = "ScriptPath=";
+	public static final String script_off = "UseInitScript=0";
+	public static final String font_9 = "FontSize=9";
+	public static final String userName_off="UserName=root";
+	public static final String userKey_off="UserKey=mouyu_id_rsa";
+	
+	
+	public static final String sd_flag="ForwardToXmanager=1";
+	
+	public static final String mysql_sd_1_add="FwdReq_0_Incoming=0";
+	public static final String mysql_sd_2_add="FwdReq_0_Description=";
+	public static final String mysql_sd_3_add="FwdReq_0_Source=localhost";
+	public static final String mysql_sd_4_add="FwdReq_0_Port=41001";//根据需要更换
+	public static final String mysql_sd_5_add="FwdReq_0_LocalOnly=1";
+	public static final String mysql_sd_6_add="FwdReq_0_Host=localhost";
+	//配置mysql和mongoDB隧道，所以个数为2。localhost:41001-->localhost:3306,localhost:40001-->localhost:40000
+	public static final String mysql_sd_7_off="FwdReqCount=0";
+	public static final String mysql_sd_8_add="FwdReq_0_HostPort=3306";
+	
+	public static final String mongo_sd_1_add="FwdReq_1_Incoming=0";
+	public static final String mongo_sd_2_add="FwdReq_1_Description=";
+	public static final String mongo_sd_3_add="FwdReq_1_Source=localhost";
+	public static final String mongo_sd_4_add="FwdReq_1_Port=40001";//根据需要更换
+	public static final String mongo_sd_5_add="FwdReq_1_LocalOnly=1";
+	public static final String mongo_sd_6_add="FwdReq_1_Host=localhost";
+	//配置mysql和mongoDB隧道，所以个数为2。localhost:41001-->localhost:3306,localhost:40001-->localhost:40000
+	public static final String mongo_sd_7_off="FwdReqCount=0";
+	public static final String mongo_sd_8_add="FwdReq_1_HostPort=40000";
+}
--- src/com/sail2world/main/Main.java
+++ src/com/sail2world/main/Main.java
@@ -1,57 +1,97 @@
-package com.sail2world.main;
+ï»¿package com.sail2world.main;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 
+import com.sail2world.cons.Constants;
+
 public class Main {
 
-	public static String root="ScriptPath=C:\\Users\\Administrator\\Documents\\NetSarang\\Xshell\\Scripts\\ScriptSample\\Session\\xshell_session_login.vbs";
-	public static String font_14="FontSize=14";
-	public static String script_on="UseInitScript=1";
 	public static void main(String[] args) throws IOException {
-		
-		String path="C:\\Users\\Administrator\\Documents\\NetSarang\\Xshell\\Sessions\\ÁúÖéQ´«\\¹ú·þ\\°²×¿\\(°²×¿321·þ-322·þ)119.29.27.98.xsh";
-		String script="ScriptPath=";
-		String script_off="UseInitScript=0";
-		String font_9="FontSize=9";
-		File f=new File(path);
-		List<String> list=getContent(f);
-		for (int i = 0; i < list.size(); i++) {
-			//TODO£ºÉèÖÃµÇÂ¼¹«Ô¿
-			
-			//TODO£ºÉèÖÃËíµÀ
-			
-			//ÉèÖÃ´ò¿ª»á»°¼´ÊÇrootÈ¨ÏÞ
-			replace(f, list, i,script_off,script_on);
-			replace(f, list, i,script,root);
-			//ÉèÖÃ×ÖÌåÎª14ºÅ£¬ÊÊºÏ²é¿´ÈÕÖ¾
-			replace(f, list, i,font_9,font_14);
-			
-			
-		}
-		
+
+		File dny_f = new File(Constants.dny_path);
+		String[] ext = { "xsh" };
+//		Collection<File> dny_files = FileUtils.listFiles(dny_f, ext, true);
+		Collection<File> dny_files = new ArrayList<File>();
+		dny_files.add(new File(Constants.dny_temp_path));
+		happy_dny(Constants.userName_off, Constants.userKey_off,
+				Constants.script, Constants.script_off, Constants.font_9,
+				dny_files);
+
 	}
 
+	/**
+	 * @param script
+	 * @param script_off
+	 * @param font_9
+	 * @param dny_files
+	 * @throws IOException
+	 */
+	private static void happy_dny(String userName_off, String userKey_off,
+			String script, String script_off, String font_9,
+			Collection<File> dny_files) throws IOException {
+		for (File f : dny_files) {
+			List<String> list = getContent(f);
+			for (int i = 0; i < list.size(); i++) {
+				// è®¾ç½®ç™»å½•å…¬é’¥
+//				replace(f, list, i, userName_off, Constants.userName_on);
+//				replace(f, list, i, userKey_off, Constants.userKey_on);
+				// è®¾ç½®éš§é“
+				add(f, list, i);
+				replace(f, list, i, Constants.mysql_sd_7_off, Constants.mysql_sd_7_on);
+				// // è®¾ç½®æ‰“å¼€ä¼šè¯å³æ˜¯rootæƒé™
+				// replace(f, list, i, script_off, script_on);
+				// replace(f, list, i, script, root);
+				// // è®¾ç½®å­—ä½“ä¸º14å·ï¼Œé€‚åˆæŸ¥çœ‹æ—¥å¿—
+				// replace(f, list, i, font_9, font_14);
+
+			}
+			System.out.println(f.getName() + "--->âˆš");
+		}
+	}
+
+	private static void add(File f, List<String> list, int i)throws IOException{
+		if (list.get(i).equals(Constants.sd_flag)) {
+			list.add(i+1, Constants.mysql_sd_1_add);
+			list.add(i+2, Constants.mysql_sd_2_add);
+			list.add(i+3, Constants.mongo_sd_1_add);
+			list.add(i+4, Constants.mongo_sd_2_add);
+			list.add(i+5, Constants.mysql_sd_3_add);
+			list.add(i+6, Constants.mongo_sd_3_add);
+			list.add(i+7, Constants.mysql_sd_4_add);
+			list.add(i+8, Constants.mysql_sd_5_add);
+			list.add(i+9, Constants.mysql_sd_6_add);
+			list.add(i+10, Constants.mongo_sd_4_add);
+			list.add(i+11, Constants.mongo_sd_5_add);
+			list.add(i+12, Constants.mongo_sd_6_add);
+			list.add(i+13, Constants.mysql_sd_8_add);
+			list.add(i+14, Constants.mongo_sd_8_add);
+			FileUtils.writeLines(f, list);
+		}
+	}
+	
+	
 	/**
 	 * @param f
 	 * @param list
 	 * @param i
 	 * @throws IOException
 	 */
-	public static void replace(File f, List<String> list, int i,String originInfo,String replaceInfo)
-			throws IOException {
+	private static void replace(File f, List<String> list, int i,
+			String originInfo, String replaceInfo) throws IOException {
 		if (list.get(i).equals(originInfo)) {
 			list.remove(i);
-			list.add(i,replaceInfo);
+			list.add(i, replaceInfo);
 			FileUtils.writeLines(f, list);
 		}
 	}
-	
-	private  static List<String> getContent(File f) throws IOException{
+
+	private static List<String> getContent(File f) throws IOException {
 		return FileUtils.readLines(f);
 	}
 
